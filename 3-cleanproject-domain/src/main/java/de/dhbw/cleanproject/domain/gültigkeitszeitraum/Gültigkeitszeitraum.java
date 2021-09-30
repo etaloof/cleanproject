9e23dfdb326ee589a84fe7229f955022d632a93f
Stefan Lukas <etaloof@gmail.com>
2021-09-30 20:03:54 +0200

    Fix value object

--- 3-cleanproject-domain/src/main/java/de/dhbw/cleanproject/domain/gültigkeitszeitraum/Gültigkeitszeitraum.java
+++ 3-cleanproject-domain/src/main/java/de/dhbw/cleanproject/domain/gültigkeitszeitraum/Gültigkeitszeitraum.java
@@ -1,17 +1,18 @@
 package de.dhbw.cleanproject.domain.gültigkeitszeitraum;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 import javax.persistence.*;
 
 @Embeddable
 @Table(name = "gültigkeitszeitraum>")
-public class Gültigkeitszeitraum {
+public final class Gültigkeitszeitraum {
     @Column(name = "gültig_ab")
-    private LocalDate gültigAb;
+    private LocalDate gültigAb; // not final for JPA
 
     @Column(name = "gültig_bis")
-    private LocalDate gültigBis;
+    private LocalDate gültigBis; // not final for JPA
 
     public LocalDate getGültigAb() {
         return gültigAb;
@@ -27,5 +28,18 @@ public class Gültigkeitszeitraum {
     public Gültigkeitszeitraum(LocalDate gültigAb, LocalDate gültigBis) {
         this.gültigAb = gültigAb;
         this.gültigBis = gültigBis;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gültigkeitszeitraum that = (Gültigkeitszeitraum) o;
+        return gültigAb.equals(that.gültigAb) && gültigBis.equals(that.gültigBis);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gültigAb, gültigBis);
     }
 }
